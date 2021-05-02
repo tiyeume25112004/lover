19eb574cb9c2dd44ccdf5445c824739bf21ebf52
tiyeume25112004 <77606941+tiyeume25112004@users.noreply.github.com>
2021-05-02 09:30:15 +0700

    Create love.c

---
+++ love.c
@@ -0,0 +1,176 @@
+tha thinh crush de ae 
+#include "stdio.h"
+#include "conio.h"
+#include "windows.h"
+int main(){
+    int i,j;
+    for(i=1;i<=100;i++)
+ {
+        system("cls");
+        printf("\n\t\t\t\tI Love You ");
+        printf("\n\n\t\t\t Send ......... to my love : :");
+        printf("\n\n\t\t\t       Loading...%d %c", i, '%');
+        printf("\n\n\t\t-----------------------------------------------");
+        if(i==10)
+ {
+            printf("\n\n\t\t\t\tDAT DEP TRAi");
+            printf("\n\n\t\t   Cau ak! Minh co vai dieu muon noi voi cau! ");
+            Sleep(2000);
+        }
+        if(i==20)
+ {
+            printf("\n\n\t\t\t\tDAT DEP TRAi");
+            printf("\n\n\t     Minh co the lam tat ca vi cau , chi can cau duoc vui !");
+            Sleep(2000);
+        }
+        if(i==30)
+ {
+            printf("\n\n\t\t\t\tDAT DEP TRAi");
+            printf("\n\n\t\t Minh nguyen lam nguoi tham lang phia sau cau !");
+            Sleep(2000);
+        }
+        if(i==40)
+ {
+            printf("\n\n\t\t\t\tDAT DEP TRAi");
+            printf("\n\n\t\t       Yeu gan hay xa khong quan trong");
+            Sleep(2000);
+        }
+        if(i==50)
+ {
+            printf("\n\n\t\t\t\tDAT DEP TRAi");
+            printf("\n\n\t     Khoang cach se khong xa neu chung ta xem nhau la tat ca !");
+            Sleep(2000);
+        }
+        if(i==60)
+ {
+            printf("\n\n\t\t\t\tDAT DEP TRAi");
+            printf("\n\n\t\t\tMinh muon ben cau ngay luc nay!");
+            Sleep(2000);
+        }
+        if(i==70)
+ {
+            printf("\n\n\t\t\t\tDAT DEP TRAi");
+            printf("\n\n\t     Minh yeu cau ! Va`....chang bao gio het yeu cau ^-^!");
+            Sleep(2000);
+        }
+        if(i==80)
+ {
+            printf("\n\n\t\t\t\tDAT DEP TRAi");
+            printf("\n\n\t\tMinh muon di cung cau het quang duong con lai ! ");
+            Sleep(2000);
+        }
+        if(i==90)
+ {
+            printf("\n\n\t\t\t\tDAT DEP TRAi");
+            printf("\n\n\t\t      Lam nguoi yeu cua minh nhe ^-^! ");
+            Sleep(2000);
+        }
+        if(i==98)
+ {
+            printf("\n\n\t\t\t\t DAT DEP TRAi!");
+            printf("\n\n\t\t\t\t I Love You!");
+            Sleep(4000);
+        }
+        if(i==99)
+ {
+            for(j=1;j<=23;j++){
+                system("cls");
+                if(j==1){
+                    printf("\t\t   *                \n");
+                    Sleep(500);
+                }
+                if(j== 2){
+                        printf("\t\t   * .                \n");
+                        Sleep(500);
+                }
+                if(j== 3){
+                        printf("\t\t   * .                \n");
+                        printf("\t\t       .              \n");
+                        Sleep(500);
+                }
+                if(j== 4){
+                        printf("\t\t   * .   .            \n");
+                        printf("\t\t       .              \n");
+                        Sleep(500);
+                }
+                if(j== 5){
+                        printf("\t\t   * .   . *         \n");
+                        printf("\t\t       .              \n");
+                        Sleep(500);
+                }
+                if(j== 6){
+                        printf("\t\t   * .   . *.         \n");
+                        printf("\t\t       .              \n");
+                        Sleep(500);
+                }
+                if(j== 7){
+                        printf("\t\t   * .   . *.         \n");
+                        printf("\t\t       .     *        \n");
+                        Sleep(500);
+                }
+                if(j== 8){
+                        printf("\t\t   * .   . *.         \n");
+                        printf("\t\t       .     *        \n");
+                        printf("\t\t             *        \n");
+                        Sleep(500);
+                }
+                if(j== 9){
+                        printf("\t\t   * .   . *.         \n");
+                        printf("\t\t       .     *        \n");
+                        printf("\t\t             *        \n");
+                        printf("\t\t           *          \n");
+                        Sleep(500);
+                }
+                if(j== 10){
+                        printf("\t\t   * .   . *.         \n");
+                        printf("\t\t       .     *        \n");
+                        printf("\t\t             *        \n");
+                        printf("\t\t           *          \n");
+                        printf("\t\t         *            \n");
+                        Sleep(500);
+                }
+                if(j== 11){
+                        printf("\t\t   * .   . *.         \n");
+                        printf("\t\t       .     *        \n");
+                        printf("\t\t             *        \n");
+                        printf("\t\t           *          \n");
+                        printf("\t\t         *            \n");
+                        printf("\t\t       *              \n");
+                        Sleep(500);
+ }
+ if(j== 12){
+                        printf("\t\t   * .   . *.         \n");
+                        printf("\t\t       .     *        \n");
+                        printf("\t\t             *        \n");
+                        printf("\t\t           *          \n");
+                        printf("\t\t    *     *            \n");
+                        printf("\t\t       *              \n");
+                        Sleep(500);}
+                        if(j== 13){
+                        printf("\t\t   * .   . *.         \n");
+                        printf("\t\t       .     *        \n");
+                        printf("\t\t             *        \n");
+                        printf("\t\t  *         *          \n");
+                        printf("\t\t    *     *            \n");
+                        printf("\t\t       *              \n");
+                        Sleep(500);}
+                        if(j== 14){
+                        printf("\t\t   * .   . *.         \n");
+                        printf("\t\t       .     *        \n");
+                        printf("\t\t *           *        \n");
+                        printf("\t\t  *         *          \n");
+                        printf("\t\t    *     *            \n");
+                        printf("\t\t       *              \n");
+                        Sleep(500);}
+                         if(j== 15){
+                        printf("\t\t . * .   . *.         \n");
+                        printf("\t\t*      .     *        \n");
+                        printf("\t\t *           *        \n");
+                        printf("\t\t  *         *          \n");
+                        printf("\t\t    *     *            \n");
+                        printf("\t\t       *              \n");
+                        Sleep(500);}
+                    }
+                }
+            }
+        }
